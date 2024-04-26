91b1699115348914a279ca4cc72640f8beccbb1f
duxingchen <duxingchen@58.com>
2024-04-26 17:44:39 +0800

    17:44

--- flask_back/Run.py
+++ flask_back/Run.py
@@ -2,13 +2,13 @@ from flask import Flask
 from flask_cors import CORS
 
 from flask_back.routes.SelectScreen import select_screen_bp
-from flask_back.routes.FeatureExtractionScreen import fe_screen_bp
+from flask_back.routes.FeatureExtractionScreen import feature_extraction_screen_bp
 
 app = Flask(__name__)
 CORS(app)
 
 app.register_blueprint(select_screen_bp, url_prefix='/api')
-app.register_blueprint(fe_screen_bp, url_prefix='/api')
+app.register_blueprint(feature_extraction_screen_bp, url_prefix='/api')
 
 if __name__ == '__main__':
     app.run(debug=True)
